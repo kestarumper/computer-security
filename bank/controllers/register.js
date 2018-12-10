bb199d8d69539fcf7157a49b220f3511d55d3232
Adrian Mucha <kestarumper8@gmail.com>
2018-12-10 10:44:41 +0100

    fix register after making vulnerable

--- bank/controllers/register.js
+++ bank/controllers/register.js
@@ -6,13 +6,15 @@ function renderRegister(req, res, next) {
 }
 
 async function handleRegistration(req, res, next) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() })
-    }
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return res.status(422).json({ errors: errors.array() })
+    // }
     const { email, name, surname, password } = req.body
-    password = await hashPassword(password)
-    const response = await insertUser(email, name, surname, password).catch((err) => {
+    const hPass = await hashPassword(password).catch((err) => {
+        return next(err)
+    })
+    const response = await insertUser(email, name, surname, hPass).catch((err) => {
         return next(new Error('Error '))
     })
     res.redirect('/login')
